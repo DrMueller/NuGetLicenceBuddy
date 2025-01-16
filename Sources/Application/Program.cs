741ba82332cf62bce5d0ca0745ea5f147f84885e
Müller Matthias <matthias.mueller@noser.com>
2025-01-16 17:54:12 +0100

    Init

---
+++ Sources/Application/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace Mmu.NuGetLicenceBuddy
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var versionString = Assembly.GetEntryAssembly()?
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion;
+
+            Console.WriteLine($"NuGetLicenseBuddy v{versionString}");
+            Console.WriteLine("-------------");
+            Console.WriteLine("\nUsage:");
+            Console.WriteLine("  botsay <message>");
+        }
+    }
+}
