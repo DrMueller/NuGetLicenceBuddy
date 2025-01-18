7c4886961649989ac3ff2652c6ff46b469c0cd89
Müller Matthias <matthias.mueller@noser.com>
2025-01-18 11:21:06 +0100

    WIP

--- Sources/Application/Areas/Outputs/Services/IOutputWriter.cs
+++ Sources/Application/Areas/Outputs/Services/IOutputWriter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mmu.NuGetLicenceBuddy.Areas.Outputs.Services
+﻿namespace Mmu.NuGetLicenceBuddy.Areas.Outputs.Services
 {
     public interface IOutputWriter
     {
--- Sources/Application/Infrastructure/Logging/Models/LogLevel.cs
+++ Sources/Application/Infrastructure/Logging/Models/LogLevel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mmu.NuGetLicenceBuddy.Infrastructure.Logging.Models
+﻿namespace Mmu.NuGetLicenceBuddy.Infrastructure.Logging.Models
 {
     public enum LogLevel
     {
