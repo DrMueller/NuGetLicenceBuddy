996e2596b249982bfe2d3c415baa04c15ea93f07
Müller Matthias <matthias.mueller@noser.com>
2025-01-27 11:18:28 +0100

    WIP

--- Sources/Application/Infrastructure/Outputs/Services/Implementation/OutputWriter.cs
+++ Sources/Application/Infrastructure/Outputs/Services/Implementation/OutputWriter.cs
@@ -1,13 +1,23 @@
 ﻿using System.IO.Abstractions;
+using Mmu.NuGetLicenceBuddy.Infrastructure.Logging.Services;
 
 namespace Mmu.NuGetLicenceBuddy.Infrastructure.Outputs.Services.Implementation
 {
-    public class OutputWriter(IFileSystem fileSystem) : IOutputWriter
+    public class OutputWriter(
+        ILoggingService logger,
+        IFileSystem fileSystem) : IOutputWriter
     {
         public async Task WriteToFileAsync(string text)
         {
             var outputPath = Environment.CurrentDirectory;
             var outputFilePath = fileSystem.Path.Combine(outputPath, "licences.txt");
+
+            logger.LogInfo($"Writing output to {outputFilePath}..");
+
+            if (!fileSystem.Directory.Exists(outputPath))
+            {
+                fileSystem.Directory.CreateDirectory(outputPath);
+            }
 
             await fileSystem.File.WriteAllTextAsync(outputFilePath, text);
         }
